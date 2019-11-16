7fa8bbd886af4408df2cf2f27460df783178879a
Pedro Henriques <pedrohn13@gmail.com>
2019-11-16 10:39:06 -0300

    Splash Screen

---
+++ app/src/main/java/br/com/phneto/kotlinissues/view/activity/SplashScreen.kt
@@ -0,0 +1,24 @@
+package br.com.phneto.kotlinissues.view.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import br.com.phneto.kotlinissues.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
